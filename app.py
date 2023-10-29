9687004f6274fd25deea6ca0a099b0a7c8df0cdf
Neil Guise <neilguise@gmail.com>
2023-10-29 19:27:03 -0400

    flask portion

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 #################################################
 # Database Setup
@@ -8,23 +10,38 @@
 
 
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///hawaii.sqlite")
+session = Session(engine)
 
 # reflect the tables
-
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start> and /api/v1.0/<start>/<end>"
+    )
+
